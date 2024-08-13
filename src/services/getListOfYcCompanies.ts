4e31760b941941f37c5dc16395bd1c5a6a42e419
Samson Ong <samson.ong@fundnel.com>
2024-08-13 15:15:30 +0800

    `try-finally` to avoid dangling page

--- src/services/getListOfYcCompanies.ts
+++ src/services/getListOfYcCompanies.ts
@@ -24,94 +24,105 @@ export async function getListOfYcCompanies({
   browser,
   batchNumber,
 }: Props): Promise<GetListOfYcCompaniesType> {
-  // * Navigate to https://www.ycombinator.com/companies?batch=X00
-  const url = new URL(`companies?batch=${batchNumber}`, YC_BASE_URL).toString();
-  consoleLog(`Navigating to ${chalk(url, "link")}...`);
   const page = await browser.newPage();
-  await page.goto(url);
-  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
-  // * Wait for page to finish loading
-  await page.waitForNetworkIdle();
+  try {
+    // * Navigate to https://www.ycombinator.com/companies?batch=X00
+    const url = new URL(
+      `companies?batch=${batchNumber}`,
+      YC_BASE_URL
+    ).toString();
+    consoleLog(`Navigating to ${chalk(url, "link")}...`);
+    await page.goto(url);
+    page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
 
-  // * This page has infinite scrolling. Scroll till the end
-  await scrollUntilPageEnd({ page });
+    // * Wait for page to finish loading
+    await page.waitForNetworkIdle();
 
-  // * Look for `<div>Showing X of X companies</div>`
-  consoleLog(
-    "Looking for `<div>Showing X of X companies</div>`...",
-    "info",
-    "dim"
-  );
-  const siblingOfCompaniesListDiv = await page
-    .locator("div ::-p-text(Showing )")
-    .waitHandle();
-  if (!siblingOfCompaniesListDiv) {
-    throw new Error("Could not find `<div>Showing X of X companies</div>`");
-  }
+    // * This page has infinite scrolling. Scroll till the end
+    await scrollUntilPageEnd({ page });
 
-  // * Grab `<div>` containing list of companies
-  consoleLog(`Grabbing <div> containing list of companies...`, "info", "dim");
-  const companiesListDiv = await siblingOfCompaniesListDiv.evaluateHandle(
-    (sibling) => {
-      const output = sibling.nextElementSibling;
-      if (!output) {
-        throw new Error("Could not find `<div>` containing list of companies");
-      }
-      return output;
+    // * Look for `<div>Showing X of X companies</div>`
+    consoleLog(
+      "Looking for `<div>Showing X of X companies</div>`...",
+      "info",
+      "dim"
+    );
+    const siblingOfCompaniesListDiv = await page
+      .locator("div ::-p-text(Showing )")
+      .waitHandle();
+    if (!siblingOfCompaniesListDiv) {
+      throw new Error("Could not find `<div>Showing X of X companies</div>`");
     }
-  );
 
-  // * Grab list of companies
-  consoleLog(`Grabbing list of companies...`, "info", "dim");
-  const companies = await companiesListDiv.evaluate((div) => {
-    const listOfCompaniesA = div.querySelectorAll("a");
-    return Array.from(listOfCompaniesA).flatMap((companyA) => {
-      // * YC Profile URL of company
-      const relativeYcProfileUrl = companyA.getAttribute("href");
-      if (!relativeYcProfileUrl) return [];
-      const ycProfileUrl = new URL(
-        relativeYcProfileUrl,
-        "https://www.ycombinator.com"
-      ).toString();
+    // * Grab `<div>` containing list of companies
+    consoleLog(`Grabbing <div> containing list of companies...`, "info", "dim");
+    const companiesListDiv = await siblingOfCompaniesListDiv.evaluateHandle(
+      (sibling) => {
+        const output = sibling.nextElementSibling;
+        if (!output) {
+          throw new Error(
+            "Could not find `<div>` containing list of companies"
+          );
+        }
+        return output;
+      }
+    );
 
-      // * This is the `<div class="lg:max-w-[90%]">` containing the company info
-      const infoDiv =
-        companyA.querySelector("div")?.lastElementChild?.firstElementChild;
-      if (!infoDiv) return [];
+    // * Grab list of companies
+    consoleLog(`Grabbing list of companies...`, "info", "dim");
+    const companies = await companiesListDiv.evaluate((div) => {
+      const listOfCompaniesA = div.querySelectorAll("a");
+      return Array.from(listOfCompaniesA).flatMap((companyA) => {
+        // * YC Profile URL of company
+        const relativeYcProfileUrl = companyA.getAttribute("href");
+        if (!relativeYcProfileUrl) return [];
+        const ycProfileUrl = new URL(
+          relativeYcProfileUrl,
+          "https://www.ycombinator.com"
+        ).toString();
 
-      // * The first two <div> have what we need
-      const [nameAndLocationDiv, descriptionDiv] =
-        infoDiv.querySelectorAll("div");
+        // * This is the `<div class="lg:max-w-[90%]">` containing the company info
+        const infoDiv =
+          companyA.querySelector("div")?.lastElementChild?.firstElementChild;
+        if (!infoDiv) return [];
 
-      // * Name & Location
-      const name = nameAndLocationDiv.firstElementChild?.textContent?.trim();
-      const location = nameAndLocationDiv.lastElementChild?.textContent?.trim();
-      if (!name || !location) return [];
+        // * The first two <div> have what we need
+        const [nameAndLocationDiv, descriptionDiv] =
+          infoDiv.querySelectorAll("div");
 
-      // * Short Description
-      const description = descriptionDiv.firstElementChild?.textContent?.trim();
-      if (!description) return [];
+        // * Name & Location
+        const name = nameAndLocationDiv.firstElementChild?.textContent?.trim();
+        const location =
+          nameAndLocationDiv.lastElementChild?.textContent?.trim();
+        if (!name || !location) return [];
 
-      return {
-        name,
-        description,
-        location,
-        ycProfileUrl,
-      };
+        // * Short Description
+        const description =
+          descriptionDiv.firstElementChild?.textContent?.trim();
+        if (!description) return [];
+
+        return {
+          name,
+          description,
+          location,
+          ycProfileUrl,
+        };
+      });
     });
-  });
 
-  // * Remember to close page!
-  await page.close();
+    consoleLog(
+      `Found ${companies.length} companies in batch ${batchNumber}\n`,
+      "success"
+    );
 
-  consoleLog(
-    `Found ${companies.length} companies in batch ${batchNumber}\n`,
-    "success"
-  );
-  return {
-    batchNumber,
-    numberOfCompanies: companies.length,
-    companies,
-  };
+    return {
+      batchNumber,
+      numberOfCompanies: companies.length,
+      companies,
+    };
+  } finally {
+    // * Remember to close page!
+    await page.close();
+  }
 }
--- src/services/getYcBatches.ts
+++ src/services/getYcBatches.ts
@@ -8,81 +8,86 @@ type Props = {
 };
 
 export async function getYcBatches({ browser }: Props): Promise<string[]> {
-  // * Navigate to https://www.ycombinator.com/companies
-  const url = new URL("companies", YC_BASE_URL).toString();
-  consoleLog(`Navigating to ${chalk(url, "link")}...`);
   const page = await browser.newPage();
-  await page.goto(url);
 
-  // * Wait for page to finish loading
-  await page.waitForNetworkIdle();
+  try {
+    // * Navigate to https://www.ycombinator.com/companies
+    const url = new URL("companies", YC_BASE_URL).toString();
+    consoleLog(`Navigating to ${chalk(url, "link")}...`);
+    await page.goto(url);
 
-  // * Look for `<h4>Batch</h4>`
-  consoleLog("Looking for `<h4>Batch</h4>`...", "info", "dim");
-  const allH4ElementsHandle = await page.$$("h4");
-  if (allH4ElementsHandle.length < 1) throw new Error("`<h4>` not found");
+    // * Wait for page to finish loading
+    await page.waitForNetworkIdle();
 
-  let matchingH4Handle: ElementHandle<HTMLHeadingElement> | null = null;
-  for (let i = 0; i < allH4ElementsHandle.length; i++) {
-    const h4 = allH4ElementsHandle[i];
-    const innerText = await h4.evaluate((el) => el.textContent?.toLowerCase());
-    if (innerText?.includes("batch")) {
-      matchingH4Handle = h4;
-      break;
+    // * Look for `<h4>Batch</h4>`
+    consoleLog("Looking for `<h4>Batch</h4>`...", "info", "dim");
+    const allH4ElementsHandle = await page.$$("h4");
+    if (allH4ElementsHandle.length < 1) throw new Error("`<h4>` not found");
+
+    let matchingH4Handle: ElementHandle<HTMLHeadingElement> | null = null;
+    for (let i = 0; i < allH4ElementsHandle.length; i++) {
+      const h4 = allH4ElementsHandle[i];
+      const innerText = await h4.evaluate((el) =>
+        el.textContent?.toLowerCase()
+      );
+      if (innerText?.includes("batch")) {
+        matchingH4Handle = h4;
+        break;
+      }
     }
+    if (!matchingH4Handle) throw new Error("`<h4>Batch</h4>` not found");
+
+    // * Go through all `<div>` siblings of `<h4>Batch</h4>` and collect batch numbers
+    consoleLog(
+      "Found `<h4>Batch</h4>`! Getting sibling `<div>`...",
+      "info",
+      "dim"
+    );
+    const batchSectionHandle = await matchingH4Handle.evaluateHandle((h4) => {
+      const parent = h4.parentElement as HTMLDivElement;
+      if (!parent) throw new Error("`<h4>Batch</h4>` is an orphan");
+      return parent;
+    });
+
+    // * Press "<a>See all options</a>" to reveal all batches
+    consoleLog(
+      "Clicking `<a>See all options</a>` to reveal all batches...",
+      "info",
+      "dim"
+    );
+    const seeAllOptions = await batchSectionHandle.evaluateHandle((section) => {
+      const seeAllOptions = section.querySelector("a");
+      if (!seeAllOptions) throw new Error("`<a>See all options</a>` not found");
+      return seeAllOptions;
+    });
+    await seeAllOptions.click();
+
+    // * Getting all sibling `<div>` elements
+    consoleLog("Getting all sibling `<div>` elements...", "info", "dim");
+    const siblingDivsHandle = await batchSectionHandle.evaluateHandle(
+      (batchSection) => {
+        const siblingDivs = batchSection.querySelectorAll("div");
+        if (!siblingDivs) throw new Error("`<h4>Batch</h4>` has no siblings");
+        return siblingDivs;
+      }
+    );
+
+    // * Extracting batch numbers
+    consoleLog("Extracting batch numbers...", "info", "dim");
+    const batchNumbers: string[] = await siblingDivsHandle.evaluate(
+      (siblingDivs) =>
+        Array.from(siblingDivs).flatMap(
+          (div) => div.querySelector("span")?.textContent ?? []
+        )
+    );
+
+    consoleLog(
+      `${batchNumbers.length} batches found (${batchNumbers[0]} ~ ${batchNumbers[batchNumbers.length - 1]})\n`,
+      "success"
+    );
+    return batchNumbers;
+  } finally {
+    // * Remember to close page!
+    await page.close();
   }
-  if (!matchingH4Handle) throw new Error("`<h4>Batch</h4>` not found");
-
-  // * Go through all `<div>` siblings of `<h4>Batch</h4>` and collect batch numbers
-  consoleLog(
-    "Found `<h4>Batch</h4>`! Getting sibling `<div>`...",
-    "info",
-    "dim"
-  );
-  const batchSectionHandle = await matchingH4Handle.evaluateHandle((h4) => {
-    const parent = h4.parentElement as HTMLDivElement;
-    if (!parent) throw new Error("`<h4>Batch</h4>` is an orphan");
-    return parent;
-  });
-
-  // * Press "<a>See all options</a>" to reveal all batches
-  consoleLog(
-    "Clicking `<a>See all options</a>` to reveal all batches...",
-    "info",
-    "dim"
-  );
-  const seeAllOptions = await batchSectionHandle.evaluateHandle((section) => {
-    const seeAllOptions = section.querySelector("a");
-    if (!seeAllOptions) throw new Error("`<a>See all options</a>` not found");
-    return seeAllOptions;
-  });
-  await seeAllOptions.click();
-
-  // * Getting all sibling `<div>` elements
-  consoleLog("Getting all sibling `<div>` elements...", "info", "dim");
-  const siblingDivsHandle = await batchSectionHandle.evaluateHandle(
-    (batchSection) => {
-      const siblingDivs = batchSection.querySelectorAll("div");
-      if (!siblingDivs) throw new Error("`<h4>Batch</h4>` has no siblings");
-      return siblingDivs;
-    }
-  );
-
-  // * Extracting batch numbers
-  consoleLog("Extracting batch numbers...", "info", "dim");
-  const batchNumbers: string[] = await siblingDivsHandle.evaluate(
-    (siblingDivs) =>
-      Array.from(siblingDivs).flatMap(
-        (div) => div.querySelector("span")?.textContent ?? []
-      )
-  );
-
-  // * Remember to close page!
-  await page.close();
-
-  consoleLog(
-    `${batchNumbers.length} batches found (${batchNumbers[0]} ~ ${batchNumbers[batchNumbers.length - 1]})\n`,
-    "success"
-  );
-  return batchNumbers;
 }
