75c3de91ff1cc6c508859cfc1e0550f6a85e8b96
Samson Ong <samson.ong@fundnel.com>
2024-08-13 15:42:36 +0800

    Refactor

--- src/services/terminal/consoleLog.ts
+++ src/services/terminal/consoleLog.ts
@@ -2,21 +2,22 @@ import { chalk, ConsoleLevels } from "../chalk/chalk";
 
 export function consoleLog(message: string, type?: ConsoleLevels, dim?: "dim") {
   // * If `type` is not provided, don't chalk it
-  if (!type) console.info(message);
+  const output = type ? chalk(message, type, dim === "dim") : message;
 
   switch (type) {
     case "debug":
-      console.debug(chalk(message, type, dim === "dim"));
+      console.debug(output);
+      return;
+    case "warn":
+      console.warn(output);
+      return;
+    case "error":
+      console.error(output);
       return;
     case "info":
     case "success":
-      console.info(chalk(message, type, dim === "dim"));
-      return;
-    case "warn":
-      console.warn(chalk(message, type, dim === "dim"));
-      return;
-    case "error":
-      console.error(chalk(message, type, dim === "dim"));
+    default:
+      console.info(output);
       return;
   }
 }
