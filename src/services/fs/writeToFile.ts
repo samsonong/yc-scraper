fb615c069fe8c628e3c2a6385076a0c83b99c3ef
Samson Ong <contact@samsonong.com>
2024-08-13 22:13:53 +0800

    Support writing string without parsing

--- src/services/fs/writeToFile.ts
+++ src/services/fs/writeToFile.ts
@@ -6,9 +6,10 @@ type Props = {
   prettyName: string;
   filePath: string;
   data: unknown;
+  writeRaw?: true;
 };
 
-export function writeToFile({ prettyName, filePath, data }: Props) {
+export function writeToFile({ prettyName, filePath, data, writeRaw }: Props) {
   // * If filePath does not start with `output/`, prepend it
   if (!filePath.startsWith("output/")) {
     filePath = `output/${filePath}`;
@@ -25,14 +26,18 @@ export function writeToFile({ prettyName, filePath, data }: Props) {
     }
     return value;
   }
-  fs.writeFile(filePath, JSON.stringify(data, replacer, 2), (err) => {
-    if (err) {
-      throw new Error(`Error writing file: ${err}`);
-    } else {
-      consoleLog(
-        `List of ${prettyName} available in ${chalk(filePath, "link")}\n`,
-        "success",
-      );
+  fs.writeFile(
+    filePath,
+    writeRaw ? (data as string) : JSON.stringify(data, replacer, 2),
+    (err) => {
+      if (err) {
+        throw new Error(`Error writing file: ${err}`);
+      } else {
+        consoleLog(
+          `List of ${prettyName} available in ${chalk(filePath, "link")}\n`,
+          "success"
+        );
+      }
     }
-  });
+  );
 }
